20d4ee6c7e5342cb9c30c272347ff75230e78fc3
_Bare <bare@logixsr.com>
2020-08-15 22:50:48 -0400

    Drawing correctly; Quadtree methods;
    Added Quadtree builder methods.
    Added Quadtree clear method.
    Fix drawing Quadtree so it now visualizes correctly and only shows bounds that have children.
    Changed to a normal distribution for random points.
    Generating a random point set and displaying each frame(1 fps).
    Cargo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use rand::Rng;
 use sfml::{
     graphics::{
         CircleShape, Color, RectangleShape, RenderStates, RenderTarget, RenderWindow, Shape,
@@ -8,8 +7,8 @@ use sfml::{
 };
 
 //----- Settings ------//
-const WIN_W: u32 = 800;
-const WIN_H: u32 = 600;
+const WIN_W: f32 = 800.0;
+const WIN_H: f32 = 600.0;
 const MAX_CAP: usize = 2;
 
 type Rect = sfml::graphics::Rect<f32>;
@@ -22,15 +21,33 @@ struct Quadtree {
     quads: Option<Vec<Quadtree>>,
 }
 
-// Core impl for Quadtree is here, other stuff is below in a 2nd impl for everything else
 impl Quadtree {
-    fn new() -> Quadtree {
+    fn new() -> Self {
         Quadtree {
-            bounds: Rect::new(0.0, 0.0, WIN_W as f32, WIN_H as f32),
+            bounds: Rect::new(0.0, 0.0, WIN_W, WIN_H),
             capacity: MAX_CAP,
             children: Vec::with_capacity(MAX_CAP),
             quads: None,
         }
+    }
+
+    fn set_bounds(mut self, bounds: Rect) -> Self {
+        self.bounds = bounds;
+
+        self
+    }
+
+    fn set_capacity(mut self, capacity: usize) -> Self {
+        self.capacity = capacity;
+        self.children = Vec::with_capacity(capacity);
+
+        self
+    }
+
+    fn set_quads(mut self) -> Self {
+        self.quads = Some(vec![Quadtree::new().set_capacity(0); 4]);
+
+        self
     }
 
     fn insert(&mut self, location: Vector2f) -> bool {
@@ -106,19 +123,17 @@ impl Quadtree {
         children_in_range
     }
 
-    fn _remove_nearest(&mut self, location: Vector2f) {}
-}
+    fn _remove_nearest(&mut self, _location: Vector2f) {}
 
-// Render and utility functions for Quadtree, Core stuff is above.
-impl Quadtree {
-    fn draw(&self, window: &RenderWindow) {
-        let mut my_rect =
-            RectangleShape::with_size((self.bounds.width as f32, self.bounds.height as f32).into());
-        my_rect.set_position((self.bounds.left as f32, self.bounds.top as f32));
-        my_rect.set_fill_color(Color::TRANSPARENT);
-        my_rect.set_outline_color(Color::RED);
-        my_rect.set_outline_thickness(1.0);
-        window.draw_rectangle_shape(&my_rect, RenderStates::default());
+    fn clear(&mut self) {
+        self.children.clear();
+        self.quads = None;
+    }
+
+    fn draw(&self, window: &mut RenderWindow) {
+        if self.quads.is_none() && self.capacity <= 0 {
+            return;
+        }
 
         if self.quads.is_some() {
             for quad in self.quads.as_ref().unwrap().iter() {
@@ -127,8 +142,26 @@ impl Quadtree {
         }
 
         if self.children.len() > 0 {
-            let mut my_dot = CircleShape::new(1.0, 8);
-            my_dot.set_fill_color(Color::GREEN);
+            let random = &mut rand::thread_rng();
+
+            use rand::Rng;
+            let my_color = Color::rgb(
+                random.gen_range(u8::MAX / 8, u8::MAX),
+                random.gen_range(u8::MAX / 8, u8::MAX),
+                random.gen_range(u8::MAX / 8, u8::MAX),
+            );
+
+            let mut my_rect = RectangleShape::with_size(
+                (self.bounds.width - 2.0, self.bounds.height - 2.0).into(),
+            );
+            my_rect.set_position((self.bounds.left + 1.0, self.bounds.top + 1.0));
+            my_rect.set_fill_color(Color::TRANSPARENT);
+            my_rect.set_outline_color(my_color);
+            my_rect.set_outline_thickness(1.0);
+            window.draw(&my_rect);
+
+            let mut my_dot = CircleShape::new(2.0, 8);
+            my_dot.set_fill_color(my_color);
 
             for child in &self.children {
                 my_dot.set_position(*child);
@@ -139,27 +172,14 @@ impl Quadtree {
 }
 
 fn main() {
-    let mut quad_root = Quadtree::new();
-
-    {
-        // Random points for testing.
-        let mut random = rand::thread_rng();
-        let mut vectors = Vec::new();
-        for _idx in 0..1000 {
-            let rng_x = random.gen_range(0, WIN_W) as f32;
-            let rng_y = random.gen_range(0, WIN_H) as f32;
-            let my_vector = Vector2f::new(rng_x, rng_y);
-            vectors.push(my_vector);
-        }
-
-        for vector in vectors {
-            quad_root.insert(vector);
-        }
-    }
+    let mut quad_root = Quadtree::new()
+        .set_bounds(Rect::new(0.0, 0.0, WIN_W, WIN_H))
+        .set_capacity(0)
+        .set_quads();
 
     // Setup
-    let mut window = RenderWindow::new(
-        (WIN_W, WIN_H),
+    let window = &mut RenderWindow::new(
+        (WIN_W as u32, WIN_H as u32),
         "Quadtree",
         sfml::window::Style::CLOSE,
         &Default::default(),
@@ -184,7 +204,26 @@ fn main() {
         }
 
         // Update
-        {}
+        {
+            quad_root.clear();
+
+            // Random points for testing.
+            use rand_distr::{Distribution, Normal};
+            let normal_w = Normal::new(WIN_W as f32 / 2.0, WIN_W as f32 / 8.0).unwrap();
+            let normal_h = Normal::new(WIN_H as f32 / 2.0, WIN_H as f32 / 8.0).unwrap();
+            let random = &mut rand::thread_rng();
+            let mut vectors = Vec::new();
+            for _idx in 0..500 {
+                let rng_x = normal_w.sample(random) as f32;
+                let rng_y = normal_h.sample(random) as f32;
+                let my_vector = Vector2f::new(rng_x, rng_y);
+                vectors.push(my_vector);
+            }
+
+            for vector in vectors {
+                quad_root.insert(vector);
+            }
+        }
 
         // Render
         {
@@ -192,13 +231,13 @@ fn main() {
 
             // Draw
             {
-                quad_root.draw(&window);
+                quad_root.draw(window);
             }
 
             window.display();
         }
 
         // std::thread::yield_now();
-        std::thread::sleep(std::time::Duration::from_nanos(1));
+        std::thread::sleep(std::time::Duration::from_millis(1000 / 1));
     }
 }
